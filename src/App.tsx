ec005a70cc7cd444930c46f95aed6d5305f7b7ed
zahrabagheri1 <zahrabagheriuk@gmail.com>
2024-07-03 19:24:41 +0330

    add react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,19 @@
-import { GlobalStyles, Stack, ThemeProvider } from "@mui/material"
+import { GlobalStyles, ThemeProvider } from "@mui/material"
 import { theme } from "./styles/matrial"
 import { GlobalStyled } from "./styles/global"
 import { CacheProvider } from "@emotion/react"
 import { cacheRtl } from "./styles/cacheRtl"
+import { BrowserRouter } from "react-router-dom"
+
 
 const App: React.FC = (): JSX.Element => {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyles styles={GlobalStyled}>
         <CacheProvider value={cacheRtl}>
-
-
-          <Stack>سامانه نوبت دهی پزشکیار </Stack>
+        <BrowserRouter>
+          
+        </BrowserRouter>
         </CacheProvider>
       </GlobalStyles>
     </ThemeProvider>
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -4,5 +4,5 @@ export const GlobalStyled: GlobalStylesProps["styles"] = {
   "*": {
     textDecoration: "none",
     fontFamily: "Vazirmatn",
-  }
+  },
 };
